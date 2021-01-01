7900ee73f87009d85986d07808d194267ada1de5
James <James.Paris@Hcl.com>
2021-01-01 14:05:38 -0600

    Initial commit

---
+++ src/ClothingStore.java
@@ -0,0 +1,11 @@
+/*
+ * @author James Paris
+ * Date: 1/1/21
+ * Purpose: Practice File handling - Appending to a file
+ */
+public class ClothingStore {
+	
+	
+	
+	
+}
---
+++ src/StoreApp.java
@@ -0,0 +1,17 @@
+/*
+ * @author James Paris
+ * Date: 1/1/21
+ * Purpose: Practice File handling - Appending to a file
+ */
+public class StoreApp {
+
+	public static void main(String[] args) {
+		
+		//
+		System.out.println("Project created.");
+		
+		
+	}
+	
+	
+}
